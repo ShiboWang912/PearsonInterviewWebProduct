422c88eca1131bc8ea2f20073b59f69b54100c23
Betty Han <xhan38@my.centennialcollege.ca>
2022-12-01 22:06:51 -0500

    Add check box to show and hide closed incidents

--- my-new-app/src/app/components/incidents-list/incidents-list.component.ts
+++ my-new-app/src/app/components/incidents-list/incidents-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CrudService } from '../../service/crud.service';
+import { Component, OnInit } from '@angular/core'
+import { CrudService } from '../../service/crud.service'
 import { AuthService } from 'src/app/shared/auth.service'
 import { Router } from '@angular/router'
 
@@ -9,30 +9,45 @@ import { Router } from '@angular/router'
   styleUrls: ['./incidents-list.component.css'],
 })
 export class IncidentsListComponent implements OnInit {
+  allIncidents = [];
   Incidents: any = [];
+  isChecked: boolean = false;
 
-  constructor(private crudService: CrudService, private authService: AuthService, private router: Router) {}
+  constructor(private crudService: CrudService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    this.crudService.GetIncidents().subscribe((res) => {
-      console.log(res);
-      this.Incidents = res;
-    });
+    this.crudService.GetIncidents().subscribe((res: any) => {
+      console.log(res)
+      this.allIncidents = res
+      this.Incidents = res.filter((obj: any) => {
+        return obj.status.toLowerCase() !== 'closed'
+      })
+    })
   }
+  checkValue(event: any) {
+    console.log('here', event.target.checked)
+    if (event.target.checked == true) {
+      this.Incidents = this.allIncidents
+    } else {
+      this.Incidents = this.allIncidents.filter((obj: any) => {
+        return obj.status.toLowerCase() !== 'closed'
+      })
+    }
 
+  }
   delete(id: any, i: any) {
     if (!this.authService.isLoggedIn) {
-      window.alert('Access not allowed!');
-      this.router.navigate(['log-in']);
+      window.alert('Access not allowed!')
+      this.router.navigate(['log-in'])
     }
-else{
-    console.log(id);
-    if (window.confirm('Do you want to go ahead?')) {
-      this.crudService.deleteIncident(id).subscribe((res) => {
-        this.Incidents.splice(i, 1);
-      });
+    else {
+      console.log(id)
+      if (window.confirm('Do you want to go ahead?')) {
+        this.crudService.deleteIncident(id).subscribe((res) => {
+          this.Incidents.splice(i, 1)
+        })
+      }
     }
-  }
 
   }
 }
